7e470944c0bb8043363bb86d1489caa2f046255e
Nithyaranjini <121478308+Nithyaranjini@users.noreply.github.com>
2023-02-27 03:13:30 -0500

    Add files via upload

---
+++ CountOfOddNumbers.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class CountOfOddNumbers {
+	public static void main(String[] args) {
+		int count=0;
+		for(int i =0;i<=50;i++) {
+			if(i%2==1) {
+				count++;
+			}
+		}	
+		System.out.println(count);	
+		}
+	}
+
+
+
