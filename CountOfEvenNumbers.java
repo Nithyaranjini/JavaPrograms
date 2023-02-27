a535daebf7c65c950512bdf776d7460eb9bd2b6b
Nithyaranjini <121478308+Nithyaranjini@users.noreply.github.com>
2023-02-27 03:13:09 -0500

    Add files via upload

---
+++ CountOfEvenNumbers.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class CountOfEvenNumbers {
+	 
+		public static void main(String[] args) {
+			int count=0;
+			for(int i =0;i<=50;i++) {
+				if(i%2==0) {
+					count++;
+				}
+			}	
+			System.out.println(count);	
+			}
+		}
+
+
+
