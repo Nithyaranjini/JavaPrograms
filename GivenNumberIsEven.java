b607cec084b47ccc27e09a96717cfaddfc4ecf37
Nithyaranjini <121478308+Nithyaranjini@users.noreply.github.com>
2023-02-27 03:14:40 -0500

    Add files via upload

---
+++ GivenNumberIsEven.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class GivenNumberIsEven {
+	public static void main(String[] args) {
+		int a=254356;
+		if(a%2==0) {
+			System.out.println("Given number is even");
+		}
+		else {
+			System.out.println("Given number is odd");
+		}
+	}
+
+}
+
+
