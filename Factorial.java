04ccc5a3c72051e04fe01cd0d82cbccbe5179449
Nithyaranjini <121478308+Nithyaranjini@users.noreply.github.com>
2023-02-27 03:13:58 -0500

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Factorial {
+	public static void main(String[] args) {
+		int a=6;
+		int fact=1;
+		while(a>0) {
+			fact=fact*a;
+			a=a-1;
+			System.out.println(fact);
+		}
+
+
+}}
